b93506834dbc27b6156e988608dc8e7189cfe884
VengarvdWal <vengar_live@hotmail.com>
2020-09-02 16:24:07 +0200

    Radar Added

---
+++ Exho_Boot_SIM/Assets/Scripts/Cabine Instrumenten/UtilsClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Utils
+{
+	public static class UtilsClass
+	{
+		public static Vector3 GetVectorFromAngle(int angle)
+		{
+			// angle = 0 -> 360
+			float angleRad = angle * (Mathf.PI / 180f);
+			return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+		}
+	}
+
+}
