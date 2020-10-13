46776f5cb315c7ff3f9c3e4b8ec2ae0b1f6c9276
Baris <skullyurdb@gmail.com>
2020-10-13 16:41:02 +0200

    Minimale mechanics, redo
    pad script
    cabine hoogte script
    
    Door verkeerde communicatie opnieuw moeten werken en oude bestanden laten zitten

---
+++ Exho_Boot_SIM/Assets/NPC_Path.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Path : MonoBehaviour
+{
+
+    //De npc volgt floating objects die in de Transform array zijn gezet. de objecten die de npc volgt moet dus bereikbaar zijn voor de boot aanegzien het vector3.movetowards gebruikt. Dus dat betekent dat de doelwit object een float script moet hebben.
+
+    [SerializeField] Transform[] pathCheckPoints;
+    [SerializeField] Vector3 targetVector;
+    [SerializeField] int pathPoint;
+    [SerializeField] float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVector = pathCheckPoints[pathPoint].transform.position;
+
+        transform.position = Vector3.MoveTowards(this.transform.position, pathCheckPoints[pathPoint].transform.position, speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "points")
+        {
+            pathPoint++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Exho_Boot_SIM/Assets/cabineHoogte.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cabineHoogte : MonoBehaviour
+{
+    // dit is de script om de stuurcabine hoger of lager te zetten. Volgens meerdere zijn er schepen die dit kan.
+
+    [SerializeField] private int maxHeight;
+    [SerializeField] private int minHeight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Plus))
+        {
+            if(this.transform.position.y < maxHeight)
+            {
+                this.transform.position -= new Vector3(0, 0.01f, 0);
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.Minus))
+        {
+            if (this.transform.position.y > minHeight)
+            {
+                this.transform.position += new Vector3(0, 0.01f, 0);
+            }
+        }
+    }
+}
