f3f0931246e1b6cce4b3cd23d0c4943178a8d88d
Baris <skullyurdb@gmail.com>
2020-10-14 14:29:43 +0200

    Update Extras, new branch
    -Added showcase scene
    -Testing Radio
    - added test songs (will change later to radio comment and music)
    - made a new branch on git to test things out without bothering the work currently being performed on main an master

---
+++ Exho_Boot_SIM/Assets/Scripts/musicLoader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicLoader : MonoBehaviour
+{
+    [SerializeField] private const string audioName = "[SFM] Slam Jam.wav";
+
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
+    [SerializeField] private AudioClip[] musicCollection;
+    [SerializeField] private string soundPath;
+
+    private void Awake()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        soundPath = "file://" + Application.streamingAssetsPath + "/Music/";
+        StartCoroutine(LoadAudio());
+    }
+
+    private IEnumerator LoadAudio()
+    {
+        WWW request = GetAudioFromFile(soundPath, audioName);
+        yield return request;
+
+        audioClip = request.GetAudioClip();
+        audioClip.name = audioName;
+
+        PlayAudioFile();
+    }
+
+    private void PlayAudioFile()
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+        audioSource.loop = false;
+    }
+
+    private WWW GetAudioFromFile(string path, string filename)
+    {
+        string audioToLoad = string.Format(path + "{0}", filename);
+        WWW request = new WWW(audioToLoad);
+        return request;
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            
+        }
+    }
+}
